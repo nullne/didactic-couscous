1d07b4611a802032b3fdf59e1dabf8430114667a
le.yu <co.nullne@gmail.com>
2020-02-05 20:06:02 +0800

    ff

--- pool/pool.go
+++ pool/pool.go
@@ -9,63 +9,129 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"math/rand"
-	"os"
-	"path"
 	"sync/atomic"
 	"time"
 
 	"golang.org/x/time/rate"
 )
 
-type Pool struct {
-	prefillSize float64
-	poolSize    float64
-	getSize     int64
-	notShuffle  bool
+var (
+	ErrReadOnce = errors.New("the pool should only be read once")
+)
 
-	pools    [2]pool
-	recorder Recorder
+type Options struct {
+	GetPeriod   time.Duration
+	SampleRatio float64
+	GetQPS      float64
+	PutQPS      float64
+	// rate to fill the pool
+	MaxGetQPS float64
+	// rate to prefill the pool
+	MaxPutQPS float64
+	Burst     float64
+
+	ReadOnly bool
+	ReadOnce bool
+
+	NotShuffle bool
+}
+
+func (opt Options) prefillSize() float64 {
+	if opt.PutQPS >= opt.GetQPS {
+		return opt.poolSize() / opt.SampleRatio
+	} else {
+		return opt.poolSize() / opt.SampleRatio * (opt.GetQPS / opt.PutQPS)
+	}
+}
+
+func (opt Options) poolSize() float64 {
+	return opt.GetQPS * opt.GetPeriod.Seconds()
+}
+
+func (opt Options) getSize() int64 {
+	return int64(1 / opt.SampleRatio)
+}
+
+func (opt *Options) setDefault() {
+	if opt.GetPeriod == 0 {
+		opt.GetPeriod = time.Minute
+	}
+	if opt.SampleRatio <= 0 {
+		opt.SampleRatio = 1
+	}
+	if opt.GetQPS == 0 {
+		opt.GetQPS = 10
+	}
+	if opt.PutQPS == 0 {
+		opt.PutQPS = 10
+	}
+	if opt.MaxPutQPS == 0 {
+		opt.MaxPutQPS = -1
+	}
+	if opt.MaxGetQPS == 0 {
+		opt.MaxGetQPS = -1
+	}
+	if opt.Burst <= 0 {
+		opt.Burst = 1
+	}
+}
+
+type Pool struct {
+	pools      [2]pool
+	recorder   Recorder
+	remainings [][]byte
 
 	get chan []byte
 	put chan []byte
+
+	counter struct {
+		get     uint64
+		put     uint64
+		round   uint64
+		prefill uint64
+	}
+
+	logCh chan string
+
+	poolSize float64
+	getSize  int64
+	options  Options
 }
 
 func New(ctx context.Context, recorder Recorder, opt Options) *Pool {
 	opt.setDefault()
+	if opt.ReadOnce && !opt.ReadOnly {
+		panic("ReadOnly must be set to true when ReadOnce is true")
+	}
 	rand.Seed(int64(time.Now().Nanosecond()))
 	dataCh := make(chan []byte)
 	p := Pool{
-		poolSize:   opt.GetQPS * opt.GetPeriod.Seconds(),
-		get:        make(chan []byte),
-		put:        dataCh,
-		getSize:    int64(1 / opt.SampleRatio),
-		recorder:   recorder,
-		notShuffle: opt.NotShuffle,
-	}
-
-	if opt.PutQPS > opt.GetQPS {
-		p.prefillSize = p.poolSize / opt.SampleRatio
-	} else {
-		p.prefillSize = p.poolSize / opt.SampleRatio * (opt.GetQPS / opt.PutQPS)
+		poolSize: opt.poolSize(),
+		get:      make(chan []byte),
+		put:      dataCh,
+		getSize:  opt.getSize(),
+		recorder: recorder,
+		options:  opt,
+		logCh:    make(chan string, 1000),
 	}
 
 	p.pools[0] = newPool(int(p.poolSize))
 	p.pools[1] = newPool(int(p.poolSize))
-	log.Printf("prefill or read size: %v, pool size: %v\n", p.prefillSize, p.poolSize)
+
+	prefillSize := opt.prefillSize()
+	p.debug("prefill size: %v, pool size: %v\n", prefillSize, p.poolSize)
 
 	go func() {
-		log.Println("start prefill", p.prefillSize)
-		if err := p.prefill(ctx, dataCh, opt.MaxPutQPS, opt.MaxGetQPS, opt.Burst); err != nil {
-			log.Println("failed to prefill: ", err)
+		if err := p.prefill(ctx, prefillSize); err != nil {
+			p.debug("failed to prefill: ", err)
 			return
 		}
-		log.Println("finish prefill")
 		go p.fillPools(ctx, opt.MaxGetQPS, opt.Burst)
-		go p.putWorker(ctx, opt.PutQPS, opt.Burst, dataCh)
 		go p.getWorker(ctx, opt.GetQPS, opt.Burst)
+		if !opt.ReadOnly {
+			go p.putWorker(ctx, opt.PutQPS, opt.Burst)
+		}
 	}()
 	return &p
 }
@@ -88,45 +154,17 @@ func (p *Pool) Get(ctx context.Context) ([]byte, error) {
 	}
 }
 
-type Options struct {
-	GetPeriod   time.Duration
-	SampleRatio float64
-	GetQPS      float64
-	PutQPS      float64
-	// rate to fill the pool
-	MaxGetQPS float64
-	// rate to prefill the pool
-	MaxPutQPS float64
-	Burst     float64
-
-	ReadOnly bool
-	ReadOnce bool
-
-	NotShuffle bool
+func (p *Pool) debug(format string, a ...interface{}) {
+	select {
+	case p.logCh <- fmt.Sprintf(format, a...):
+	default:
+		// discard message when full
+	}
 }
 
-func (opt *Options) setDefault() {
-	if opt.GetPeriod == 0 {
-		opt.GetPeriod = time.Minute
-	}
-	if opt.SampleRatio <= 0 {
-		opt.SampleRatio = 0.01
-	}
-	if opt.GetQPS == 0 {
-		opt.GetQPS = 10
-	}
-	if opt.PutQPS == 0 {
-		opt.PutQPS = 10
-	}
-	if opt.MaxPutQPS == 0 {
-		opt.MaxPutQPS = -1
-	}
-	if opt.MaxGetQPS == 0 {
-		opt.MaxGetQPS = -1
-	}
-	if opt.Burst <= 0 {
-		opt.Burst = 1
-	}
+// return debug message channel from which debug message can be read
+func (p *Pool) DebugCh(ctx context.Context) chan string {
+	return p.logCh
 }
 
 type pool struct {
@@ -144,116 +182,180 @@ func newPool(size int) pool {
 	return p
 }
 
-func (p *Pool) prefill(ctx context.Context, ch <-chan []byte, wqps, rqps, burst float64) error {
-	var counter uint64
-	finish := make(chan error, 1)
-	go func() (err error) {
-		defer log.Println("finish real prefill")
-		defer func() {
-			finish <- err
-		}()
-		wlimiter := rate.NewLimiter(rate.Every(time.Second/time.Duration(wqps)), int(burst))
-		for {
-			select {
-			case <-ctx.Done():
-				return ctx.Err()
-			case key, ok := <-ch:
-				if !ok {
-					return errors.New("channel was closed")
-				}
-				if err := p.recorder.Put(key); err != nil {
-					return err
-				}
-				if c := atomic.AddUint64(&counter, 1); float64(c) >= p.prefillSize {
-					return nil
-				}
-			}
-			if err := wlimiter.Wait(ctx); err != nil {
-				return err
-			}
-		}
-	}()
+func (p *Pool) prefill(ctx context.Context, prefillSize float64) error {
+	p.debug("start prefill", prefillSize)
+	defer p.debug("finish prefill")
+	// var counter uint64
+	// finish := make(chan error, 1)
+	// go func() (err error) {
+	// 	defer log.Println("finish real prefill")
+	// 	defer func() {
+	// 		finish <- err
+	// 	}()
+	// 	wlimiter := rate.NewLimiter(rate.Every(time.Second/time.Duration(wqps)), int(burst))
+	// 	for {
+	// 		select {
+	// 		case <-ctx.Done():
+	// 			return ctx.Err()
+	// 		case key, ok := <-ch:
+	// 			if !ok {
+	// 				return errors.New("channel was closed")
+	// 			}
+	// 			if err := p.recorder.Put(key); err != nil {
+	// 				return err
+	// 			}
+	// 			if c := atomic.AddUint64(&counter, 1); float64(c) >= p.prefillSize {
+	// 				return nil
+	// 			}
+	// 		}
+	// 		if err := wlimiter.Wait(ctx); err != nil {
+	// 			return err
+	// 		}
+	// 	}
+	// }()
 
-	defer p.recorder.Rewind()
+	// step 1: read from existed
+	need, rcounter, err := p.needPrefill(ctx, prefillSize)
+	if err != nil {
+		return err
+	}
+	if !need {
+		return nil
+	}
 
-	var rcounter uint64
-	rlimiter := rate.NewLimiter(rate.Every(time.Second/time.Duration(rqps)), int(burst))
+	// step 2: put in MaxPutQPS
+	ctx2, cancel := context.WithCancel(ctx)
+	defer cancel()
+	go p.putWorker(ctx2, p.options.MaxPutQPS, p.options.Burst)
+
+	remains := uint64(prefillSize) - rcounter
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
 	for {
 		select {
 		case <-ctx.Done():
 			return ctx.Err()
-		case err := <-finish:
-			return err
-		default:
-		}
-		bs, err := p.recorder.GetN(100)
-		if err != nil {
-			return err
-		}
-		rcounter += uint64(len(bs))
-		if float64(rcounter) >= p.prefillSize {
-			atomic.StoreUint64(&counter, rcounter)
-			log.Println("read all from previous")
-			return nil
-		}
-		// read to EOF
-		if len(bs) < 100 {
-			atomic.StoreUint64(&counter, rcounter)
-			log.Printf("read %v from previous\n", rcounter)
-			break
-		}
-		if err := rlimiter.Wait(ctx); err != nil {
-			return err
+		case <-ticker.C:
+			if atomic.LoadUint64(&(p.counter.put)) >= remains {
+				return nil
+			}
 		}
 	}
-	return <-finish
+	return nil
 }
 
-func (p *Pool) putWorker(ctx context.Context, putQPS, burst float64, ch <-chan []byte) {
+func (p *Pool) needPrefill(ctx context.Context, prefillSize float64) (bool, uint64, error) {
+	defer p.recorder.Rewind()
+	batchSize := float64(100)
+	if batchSize > prefillSize {
+		batchSize = prefillSize
+	}
+
+	var rcounter uint64
+	rlimiter := rate.NewLimiter(rate.Every(time.Second/time.Duration(p.options.MaxGetQPS/batchSize)), int(p.options.Burst))
+	for {
+		if err := rlimiter.Wait(ctx); err != nil {
+			return false, 0, err
+		}
+		select {
+		case <-ctx.Done():
+			return false, 0, ctx.Err()
+		default:
+		}
+		bs, err := p.recorder.GetN(int64(batchSize))
+		if err != nil {
+			return false, 0, err
+		}
+		rcounter += uint64(len(bs))
+		if float64(rcounter+atomic.LoadUint64(&(p.counter.put))) >= prefillSize {
+			p.debug("the existed is enough to fill the pool")
+			return false, 0, nil
+		}
+		// read to EOF
+		if len(bs) < int(n) {
+			p.debug("read %v from the existed", rcounter)
+			break
+		}
+	}
+	return true, rcounter, nil
+}
+
+func (p *Pool) putWorker(ctx context.Context, putQPS, burst float64) {
 	limiter := rate.NewLimiter(rate.Every(time.Second/time.Duration(putQPS)), int(burst))
 	for {
+		limiter.Wait(ctx)
 		select {
 		case <-ctx.Done():
 			return
-		case data, ok := <-ch:
+		case data, ok := <-p.dataCh:
 			if !ok {
-				log.Println("finish put worker")
+				p.debug("finish put worker")
 				return
 			}
 			if err := p.recorder.Put(data); err != nil {
-				log.Println(err)
+				p.debug("failed to out into recorder: %v", err)
 				time.Sleep(time.Second)
 			}
+			atomic.AddUint64(&(p.counter.put), 1)
 		}
-		limiter.Wait(ctx)
 	}
 }
 
 func (p *Pool) getWorker(ctx context.Context, getQPS, burst float64) {
 	limiter := rate.NewLimiter(rate.Every(time.Second/time.Duration(getQPS)), int(burst))
 	idx := 0
+Outer:
 	for {
 		select {
 		case <-ctx.Done():
 			return
-		case <-p.pools[idx].readable:
+		case _, ok := <-p.pools[idx].readable:
+			if !ok {
+				break Outer
+			}
 		}
-		log.Println("start to read", idx)
+		p.debug("start to read %v", idx)
 		before := time.Now()
 		for _, key := range p.pools[idx].data {
 			if err := limiter.Wait(ctx); err != nil {
-				log.Println(err)
+				p.debug("limiter error: %v", err)
 				return
 			}
 			select {
 			case <-ctx.Done():
 				return
 			case p.get <- key:
+				atomic.AddUint64(&(p.counter.get), 1)
 			}
 		}
-		log.Println("end read", idx, time.Since(before))
+		p.debug("last %v reading %v", time.Since(before), idx)
 		p.pools[idx].writable <- struct{}{}
 		idx = (idx + 1) % 2
+	}
+
+	// remaines
+	if len(p.remainings) == 0 {
+		return
+	}
+	p.debug("read remainings")
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+		for _, key := range p.remainings {
+			if err := limiter.Wait(ctx); err != nil {
+				p.debug("limiter error: %v", err)
+				return
+			}
+			select {
+			case <-ctx.Done():
+				return
+			case p.get <- key:
+				atomic.AddUint64(&(p.counter.get), 1)
+			}
+		}
 	}
 }
 
@@ -265,10 +367,15 @@ func (p *Pool) fillPools(ctx context.Context, qps, burst float64) {
 			return
 		case <-p.pools[idx].writable:
 		}
-		log.Println("fill the pool", idx)
+		p.debug("fill the pool %v", idx)
 		//fill the pool
 		if err := p.fillPool(ctx, p.pools[idx].data, qps, burst); err != nil {
-			log.Printf("failed to fill the pool %v: %v, will try later\n", idx, err)
+			if err == ErrReadOnce {
+				p.debug("stop filling the pool for ReadOnce")
+				close(p.pools[idx].readable)
+				return
+			}
+			p.debug("failed to fill the pool %v: %v, will try later", idx, err)
 			time.Sleep(time.Second)
 			p.pools[idx].writable <- struct{}{}
 			continue
@@ -283,25 +390,25 @@ func (p *Pool) fillPools(ctx context.Context, qps, burst float64) {
 	}
 }
 
-func (p *Pool) dump(idx int) error {
-	dir, err := ioutil.TempDir("/tmp", "oss-key")
-	if err != nil {
-		return err
-	}
-	fp := path.Join(dir, "key-file")
-	f, err := os.Create(fp)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	log.Println("dump key file to ", fp, idx)
-	for _, key := range p.pools[idx].data {
-		if _, err := f.Write(append(key, '\n')); err != nil {
-			return err
-		}
-	}
-	return nil
-}
+// func (p *Pool) dump(idx int) error {
+// 	dir, err := ioutil.TempDir("/tmp", "oss-key")
+// 	if err != nil {
+// 		return err
+// 	}
+// 	fp := path.Join(dir, "key-file")
+// 	f, err := os.Create(fp)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer f.Close()
+// 	log.Println("dump key file to ", fp, idx)
+// 	for _, key := range p.pools[idx].data {
+// 		if _, err := f.Write(append(key, '\n')); err != nil {
+// 			return err
+// 		}
+// 	}
+// 	return nil
+// }
 
 func (p *Pool) fillPool(ctx context.Context, data [][]byte, qps, burst float64) error {
 	limiter := rate.NewLimiter(rate.Every(time.Second/time.Duration(qps)), int(burst))
@@ -312,10 +419,17 @@ func (p *Pool) fillPool(ctx context.Context, data [][]byte, qps, burst float64) 
 		}
 		if int64(len(bs)) != p.getSize {
 			// need rewind and get more
+			if p.opt.ReadOnce {
+				p.remainings = data[:i]
+				if err := shuffle(p.remainings); err != nil {
+					p.debug("failed to shuffle: %v", err)
+				}
+				return ErrReadOnce
+			}
 			if err := p.recorder.Rewind(); err != nil {
 				return err
 			}
-			log.Println("rewind to the beginning")
+			p.debug("rewind to the beginning")
 			bs2, err := p.recorder.GetN(p.getSize - int64(len(bs)))
 			if err != nil {
 				return err
@@ -328,17 +442,20 @@ func (p *Pool) fillPool(ctx context.Context, data [][]byte, qps, burst float64) 
 			return err
 		}
 	}
-	if p.notShuffle {
+	if p.options.NotShuffle {
 		return nil
 	}
-	//shuffle the data
+	return shuffle(data)
+}
+
+func shuffle(data [][]byte) error {
 	dest := make([][]byte, len(data))
 	perm := rand.Perm(len(data))
 	for i, v := range perm {
 		dest[v] = data[i]
 	}
-	if n := copy(data, dest); float64(n) != p.poolSize {
-		return fmt.Errorf("failed to copy, got %v, want %v", n, p.poolSize)
+	if n := copy(data, dest); float64(n) != len(data) {
+		return fmt.Errorf("failed to copy, got %v, want %v", n, len(data))
 	}
 	return nil
 }
--- pool/recorder.go
+++ pool/recorder.go
@@ -1,8 +1,14 @@
 package pool
 
+import "errors"
+
 type Recorder interface {
 	GetN(n int64) ([][]byte, error)
 	Rewind() error
 	Put([]byte) error
 	Close() error
 }
+
+var (
+	ErrEndOfRecorder = errors.New("end of recorder")
+)
